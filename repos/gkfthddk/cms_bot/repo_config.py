5f33c6d496ce873c508d26d52c80cdfd2491d92a
Yunjae Lee <yulee@lxplus764.cern.ch>
2023-09-26 13:58:04 +0200

    webhook cmsbot

---
+++ repos/gkfthddk/cms_bot/repo_config.py
@@ -0,0 +1,9 @@
+from os.path import basename,dirname,abspath
+
+CONFIG_DIR=dirname(abspath(__file__))
+GITHUB_WEBHOOK_TOKEN='U2FsdGVkX1+AJchmb+YYLVAsLyyY+gcOEnIvZimigNo='
+RUN_DEFAULT_CMS_BOT=False
+
+VALID_WEB_HOOKS=['.*']
+WEBHOOK_PAYLOAD=True
+JENKINS_SERVER="http://cmsjenkins10.cern.ch:8080/dmwm-jenkins"
