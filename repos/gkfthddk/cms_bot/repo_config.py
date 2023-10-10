50ce5fddc4d32473752284e66220baf4e6cddc1d
YunJae Lee <gkfthddk@gmail.com>
2023-10-10 21:14:49 +0900

    Update repo_config.py

--- repos/gkfthddk/cms_bot/repo_config.py
+++ repos/gkfthddk/cms_bot/repo_config.py
@@ -1,7 +1,7 @@
 from os.path import basename,dirname,abspath
 
 CONFIG_DIR=dirname(abspath(__file__))
-GITHUB_WEBHOOK_TOKEN='U2FsdGVkX1+AJchmb+YYLVAsLyyY+gcOEnIvZimigNo='
+GITHUB_WEBHOOK_TOKEN='U2FsdGVkX1/zM/avZLVVuoRhN4kw3aua07OQemQQyIl2fM8T3t/Erd3f3rroNiIt'
 RUN_DEFAULT_CMS_BOT=False
 
 VALID_WEB_HOOKS=['.*']
